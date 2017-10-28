dad3c91ea799328adc569c3019f10f80fafca1e3
mrbarge <matt.bargenquast@gmail.com>
2017-10-28 20:32:55 +1100

    added php

---
+++ greet.php
@@ -0,0 +1,34 @@
+<html>
+<body>
+<?php
+
+  if (!isset($_GET['name'])) {
+    return;
+  }
+  $visitor = $_GET['name'];
+
+  $config_file = getenv('APP_CONFIG');
+  if ($config_file) {
+    $appcfg = parse_ini_file($config_file);
+  } else {
+    die('Config file not found.');
+  }
+
+  $db = new PDO('mysql:host=' . $appcfg['MYSQL_DATABASE_HOST'] . ';dbname=' . $appcfg['MYSQL_DATABASE_NAME'] . ';charset=utf8',
+    $appcfg['MYSQL_DATABASE_USER'], $appcfg['MYSQL_DATABASE_PASSWORD']);
+  $stmt = $db->prepare("SELECT * FROM visitors WHERE username=?");
+  $stmt->execute(array($visitor));
+  $visits = 0;
+  if ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    $visits = $row['visits'];
+    $stmt = $db->prepare("UPDATE visitors SET visits = visits + 1 WHERE username = ?");
+    $stmt->execute(array($visitor));
+  } else {
+    $stmt = $db->prepare("INSERT INTO visitors VLAUES (?,?)");
+    $stmt->execute(array($visitor,1));
+    $visits = 1;
+  }
+  echo "Hey there $visitor you've visited $visits times.";
+?>
+</body>
+</html>
